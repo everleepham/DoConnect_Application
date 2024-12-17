dbd2e84d5ca5e68dc2959c7a851d660d8a9e0204
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-12-17 10:59:21 +0100

    update test doctor

--- back_end/doc-patients/src/test/java/com/pham/doconnect/doctors/controller/DoctorsControllerTest.java
+++ back_end/doc-patients/src/test/java/com/pham/doconnect/doctors/controller/DoctorsControllerTest.java
@@ -2,9 +2,7 @@ package com.pham.doconnect.doctors.controller;
 
 
 import com.pham.doconnect.doctors.dto.DoctorsDTO;
-import com.pham.doconnect.doctors.model.Doctors;
 import com.pham.doconnect.doctors.service.DoctorsService;
-import com.pham.doconnect.patients.service.PatientsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -70,6 +68,74 @@ public class DoctorsControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].is_active").value(true));
 
                 //not returning thỉrd doctor because is_active = false
+    }
+    @Test
+    public void testGetDoctorById() throws Exception {
+        when(doctorsService.getDoctorById(1L)).thenReturn(doctorDTO1);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/doctors/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Tim"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("timdoe@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.specialize").value(DENTIST))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.is_active").value(true));
+    }
+
+    private static final String DOCTOR_JSON = "{\"firstName\":\"Minh\", \"lastName\":\"Pham\", \"email\":\"minhpham2101@gmail.com\", \"specialize\":GENERAL, \"isActive\":false}";
+
+
+    @Test
+    public void testCreateDoctor() throws Exception {
+        when(doctorsService.saveDoctors(doctorDTO3)).thenReturn(doctorDTO3);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/doctors")
+                    .contentType("application/json")
+                    .content(DOCTOR_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Minh"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Pham"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("minhpham2101@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.specialize").value(GENERAL))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.is_active").value(false));
+
+    }
+
+    private static final String UPDATE_DOCTOR = "{\"firstName\":\"Minh\", \"lastName\":\"Pham\", \"email\":\"minhpham2101@gmail.com\", \"specialize\":GENERAL, \"isActive\":true}";
+
+    @Test
+    public void testUpdateDoctorStatus() throws Exception {
+        when(doctorsService.updateDoctorStatus(3L, doctorDTO3)).thenReturn(doctorDTO3);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/doctors/3")
+                        .contentType("application/json")
+                        .content(UPDATE_DOCTOR))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Minh"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Pham"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("minhpham2101@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.specialize").value(GENERAL))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.is_active").value(true));
+    }
+
+    @Test
+    public void searchDoctorsByName() throws Exception {
+        when(doctorsService.searchDoctorsByName("Tim")).thenReturn(List.of(doctorDTO1));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/doctors/search?name=Tim"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Tim"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("timdoe@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].specialize").value(DENTIST))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].is_active").value(true));
+    }
+
+    @Test public void searchDoctorsBySpecialize() throws Exception {
+        when(doctorsService.searchDoctorBySpecialize("pediatric")).thenReturn(List.of(doctorDTO2));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/doctors/search?specialize=Pediatric"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Carla"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("Smith"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("carla@outlook.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].specialize").value(PEDIATRIC))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].is_active").value(true));
     }
 
 }
